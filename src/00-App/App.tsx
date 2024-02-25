e2fa57eaf93269203f88ebf4dbee8378ffadb968
agro40tech <volkovvova67@gmail.com>
2024-02-25 20:32:39 +0400

    Подготовил структуру проекта

---
+++ src/00-App/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { HomePage } from "01-Pages/Home";
+
+import "./assets/Variables.css";
+import "./assets/GlobalStyle.css";
+
+export const App: FC = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
